53521ee05f3a534d3de8911202b7a444718771a2
40943257 <73061142+40943257@users.noreply.github.com>
2023-05-01 16:31:39 +0800

    除掉打錯變數 及function會呼叫多次

--- public/javascripts/link.js
+++ public/javascripts/link.js
@@ -11,23 +11,31 @@ const cameraPeer = new Peer(undefined, {
     secure: true,
     port: '3003',
 })
+var access = false
 
 socket.on('connect', () => {
     console.log('connect')
-    if(myPeer._id && cameraPeer._id)
+    if(myPeer._id && cameraPeer._id && !access){
+        access = true
         linkSuss()
+    }
 })
 
 myPeer.on('open', id => {
-    if(cameraPeer._id && socket.connecte)
+    if(cameraPeer._id && socket.connected && !access){
+        access = true
         linkSuss()
+    }
 })
 
 cameraPeer.on('open', id => {
-    if(myPeer._id && socket.connecte)
+    if(myPeer._id && socket.connected && !access){
+        access = true
         linkSuss()
+    }
 })
 const linkSuss = () => {
+    console.log('access')
     var script = document.createElement('script')
     script.src = '/javascripts/script.js'
     document.body.appendChild(script)
